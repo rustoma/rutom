ed81dc8159a03b99a441cff53cc5a2441916f677
RusinTomasz <tomaszrusin90@gmail.com>
2022-09-15 07:40:01 +0200

    chore: extend jest config

--- packages/rutom-core/jest.config.js
+++ packages/rutom-core/jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+
+    // A list of paths to directories that Jest should use to search for files in
+    // https://jestjs.io/docs/configuration#roots-arraystring
+    roots: ['<rootDir>/src/components/'],
+
     testEnvironment: "jsdom",
 
     // Jest transformations
@@ -7,5 +12,40 @@ module.exports = {
         '^.+\\.tsx?$': 'ts-jest' // Transform TypeScript files using ts-jest
     },
 
+    // A list of paths to modules that run some code to configure or set up the testing framework before each test file in the suite is executed
+    // https://jestjs.io/docs/configuration#setupfilesafterenv-array
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 
+    // Code coverage config
+    // https://jestjs.io/docs/configuration#collectcoveragefrom-array
+    coverageDirectory: '<rootDir>/coverage/',
+    collectCoverageFrom: [
+        '<rootDir>/src/components/**/*.{ts,tsx}',
+        '!<rootDir>/src/components/index.ts',
+        '!<rootDir>/src/components/Button/index.ts',
+        '!**/__mocks__/**',
+        '!**/node_modules/**',
+        '!**/*.d.ts'
+    ],
+
+    // Important: order matters, specific rules should be defined first
+    // https://jestjs.io/fr/docs/configuration#modulenamemapper-objectstring-string--arraystring
+    moduleNameMapper: {
+        // Handle CSS imports (with CSS modules)
+        // https://jestjs.io/docs/webpack#mocking-css-modules
+        '^.+\\.module\\.(css|sass|scss|less)$': 'identity-obj-proxy',
+
+        // Handle CSS imports (without CSS modules)
+        '^.+\\.(css|sass|scss|less)$': '<rootDir>/__mocks__/styleMock.js',
+
+        // Handle static assets
+        // https://jestjs.io/docs/webpack#handling-static-assets
+        '^.+\\.(jpg|jpeg|png|gif|webp|avif|svg|ttf|woff|woff2)$': `<rootDir>/__mocks__/fileMock.js`,
+
+        // Handle TypeScript path aliases
+        '^@/(.*)$': '<rootDir>/src/$1'
+    },
+
+    verbose: true,
+    testTimeout: 30000
 }
